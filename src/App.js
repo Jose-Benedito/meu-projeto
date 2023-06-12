ac8917e6d03252fbd33247f1f5a6bb9e08a891a7
jose-benedito <benethowen@yahoo.com.br>
2023-06-12 01:05:13 -0300

    Usando props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import HelloWorld  from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Benedito'
+  const name = 'Maria'
   const url = 'https://via.placeholder.com/150'
+  const dono = 'Benedito'
 
   function sum(a,b){
     return a*b
@@ -12,10 +15,20 @@ function App() {
   return (
     <div className="App">
     <h1>Olá react!</h1>
-    <p>Olá meu caro {name} !</p>
+    <p>Olá meu caro {dono} !</p>
     <p>A multiplicação de 5 e 68 é {sum(5,68)}</p>
     <img src={url} alt="imagem teste"/>
+
     <HelloWorld/>
+    <SayMyName nome="Benedito" />
+    <SayMyName nome="José" />
+    <SayMyName nome={name} />
+
+    <Pessoa 
+      nome="Rodrigo"
+      idade="28"
+      profissao="Programador"
+      foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
