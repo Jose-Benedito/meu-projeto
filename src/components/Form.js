3755e2f190981ba88c5d06e829aff8e9b81fbcae
jose-benedito <benethowen@yahoo.com.br>
2023-06-17 19:47:41 -0300

    Utilizando  useState (hook do React)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,42 @@
+import { useState } from "react"
+
 function Form(){
     function cadastrarUsuario(e){
         e.preventDefault() // Para a requisição do formulário ṕara exibir a mensagem
-        console.log('Cadastrou o Usuário!')
+        console.log(name)
+        console.log(password)
+        console.log(`Cadastrou o Usuário ${name} e senha ${password}`) //obs: a crase
     }
+
+    const [name, setName] = useState() //pega o valor do input no click(onChange)
+  //  const [name, setName] = useState("Benedito") // para valor default
+    const [password, setPassword] = useState()
     return (
         <div>
             <h1>Meu Cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"/>
+                    <label htmlFor="name">Name:</label>
+
+                    <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Digite o seu nome"
+                 //valor default   value={name}
+                    onChange={(e) => setName(e.target.value)} //
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+
+                    <input 
+                    type="password" 
+                    id="senha" 
+                    name="senha" 
+                    placeholder="Digite a sua senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"/>
